d26738de0e80bb342a852617849eacd4b7f06a30
RosarioManny <rosario.emm47@gmail.com>
2024-09-26 18:59:19 -0400

    Views,Connections

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-// EXPRESS
+// CONST
 const express = require("express");
 const app = express();
-// DOTENV
 const dotenv = require("dotenv")
-dotenv.config();
-// MONGO
 const mongoose = require("mongoose")
+// CALLING FUNCTIONS
+dotenv.config();
 mongoose.connect(process.env.MONGODB_URI) // Connection to MONGODB Database
+
+// vvvvvvvvvvvvvvvvvvv CONNECTION CHECK vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+
+app.listen(3000, ()=> {
+    console.log("Listening to port 3000")
+})
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
+})
+// vvvvvvvvvvvvvvvvvvv ROUTES vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+
+app.get("/", async (req, res) => {
+    res.render("index.ejs")
+});
